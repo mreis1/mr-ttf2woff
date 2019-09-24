c49541a9cab07b9e1d29487c56694af594548844
Marcio Reis <marcio.reis@outlook.com>
2019-09-24 14:42:21 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# mr-ttf2woff
+
+Generates woff2 fonts from ttf fonts.
+
+
+### How to use
+
+$ npm install mr-ttf2woff --src=./folder/*.ttf --out=${file}
+
+
+### Dependencies
+- fs-extra
+- ttf2woff
+- glob
+
+
+### Author
+Márcio Reis - hello[at]marcioreis.pt
+
---
+++ index.js
@@ -0,0 +1,41 @@
+const argv = require('yargs').argv
+var glob = require("glob")
+var path = require("path");
+var fs = require('fs');
+var ttf2woff2 = require('ttf2woff2');
+var fse = require('fse');
+
+if (!argv.src) {
+	throw new Error('Invalid src path');
+}
+if (!argv.outDir) {
+	argv.outDir = '.';
+	//throw new Error('Invalid output directory');
+}
+
+glob(argv.src, {}, function (err, files) {
+	if (err){
+		throw err;
+	}
+
+	var baseDir = process.cwd(), argv.outDir;
+	
+	if (files.length) {
+		fse.ensureDirSync(baseDir);
+	}
+	
+	// files is an array of filenames.
+  	// If the `nonull` option is set, and nothing
+  	// was found, then files is ["**/*.js"]
+  	// er is an error object or null.
+	files.forEach(function(file){
+		var f = path.parse(file);
+		console.log(f);
+		var input = fs.readFileSync(file);
+		console.log('Processing ' + file)
+		var outputTo = path.join(baseDir, f.name + '.woff2');
+		fs.writeFileSync(outputTo, ttf2woff2(input));		
+		console.log('>>> ' + outputTo);
+	})
+	console.log('Completed!')
+})
